fce2b1f862ed2b8d651b9ef26e947c8e92a3826b
parsaaba <parsa.ba.a@gmail.com>
2022-04-22 17:14:22 +0430

    nft realstates

---
+++ eth-contracts/secret-parameters.js
@@ -0,0 +1,13 @@
+// Secret parameters file
+// METAMASK seed (mnemonic)
+const mnemonic = "xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx";
+
+//const mnemonic = "bread mom hard denial control merit carpet easy eternal faculty square upset";
+// Infura PROJECT ID (infuraKey)
+const infuraKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+
+module.exports = {
+    mnemonic: mnemonic,
+    infuraKey: infuraKey
+};
